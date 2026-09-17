28f8aa2035d2683db67c854d85de4adb8a94d0fa
Siddhant borase <69421219+SIDDHANTBORASE@users.noreply.github.com>
2026-09-17 23:11:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // --- 1. CONNECT TO SUPABASE ---
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://phzcdsftwmjjzmtyeqps.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBoemNkc2Z0d21qanptdHllcXBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk2NjQ2NjgsImV4cCI6MjEwNTI0MDY2OH0.Oz4uYKQSOYitRe8yu27za_Zziif9xLgeTHd0hkpG6eM';
 
 const { createClient } = supabase;
 const _supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
